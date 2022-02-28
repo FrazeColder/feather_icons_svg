5e40dca4293ff43d9b6759f747b699a068f2a9ae
Jan Trier <jan.trier03@gmail.com>
2022-02-28 13:08:05 +0100

    Downgrade flutter_svg to 0.23.0+1 because of dependencies

--- lib/feather_icons_svg.dart
+++ lib/feather_icons_svg.dart
@@ -116,6 +116,7 @@ class FeatherIcon extends StatelessWidget {
 extension HexColor on Color {
   /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
   String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      //'${alpha.toRadixString(16).padLeft(2, '0')}'
       '${red.toRadixString(16).padLeft(2, '0')}'
       '${green.toRadixString(16).padLeft(2, '0')}'
       '${blue.toRadixString(16).padLeft(2, '0')}';
